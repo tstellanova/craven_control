20fdc998fd8494c911655c55d4afddecdd9aa86e
Todd Stellanova <tstellanova@users.noreply.github.com>
2026-01-07 22:03:48 -0800

    reuse ctx; read 3 nodes in a loop

--- src/main.rs
+++ src/main.rs
@@ -72,10 +72,9 @@ async fn set_pyro_current_loop_drive(ctx: &mut tokio_modbus::client::Context, mi
 
     let out_ma_setting: u16 = (milliamps/0.1).round() as u16;
     println!("writing val of : {milliamps:?} mA  -> {out_ma_setting:?}");
-    let w_rsp = ctx.write_single_register(REG_ADDR_DRIVE_MILLIAMPS, out_ma_setting).await?;
-    println!("w_rsp: {w_rsp:?}");
+    ctx.write_single_register(REG_ADDR_DRIVE_MILLIAMPS, out_ma_setting).await??;
     let read_rsp: Vec<u16> = ctx.read_holding_registers(REG_ADDR_DRIVE_MILLIAMPS, 1).await??;
-    println!("new val read_rsp: {read_rsp:?}");
+    // println!("new val read_rsp: {read_rsp:?}");
 
     Ok(())
 }
@@ -88,25 +87,25 @@ async fn set_precision_current_drive(ctx: &mut tokio_modbus::client::Context, mi
     const REG_ADDR_DRIVE_MILLIAMPS: u16  = 0x10;
     const REG_ADDR_MONITOR_MILLIAMPS: u16  = 0x11;
 
-    println!("set_precision_current_drive: {milliamps:?} mA");
+    // println!("set_precision_current_drive: {milliamps:?} mA");
 
-    println!("Reading existing drive current value");
+    // println!("Reading existing drive current value");
     let read_rsp: Vec<u16> = ctx.read_holding_registers(REG_ADDR_DRIVE_MILLIAMPS, 1).await??;
-    println!("existing current read_rsp: {read_rsp:?}");
-    // let original_ma_val = read_rsp[0];
+    // println!("existing current read_rsp: {read_rsp:?}");
+    let original_ma_val = read_rsp[0];
 
     let out_ma_setting: u16 = (milliamps/0.1).round() as u16;
-    println!("writing val of : {}", out_ma_setting);
-    let w_rsp = ctx.write_single_register(REG_ADDR_DRIVE_MILLIAMPS, out_ma_setting).await?;
-    println!("w_rsp: {w_rsp:?}");
+    // println!("writing val of : {}", out_ma_setting);
+    ctx.write_single_register(REG_ADDR_DRIVE_MILLIAMPS, out_ma_setting).await??;
     let read_rsp: Vec<u16> = ctx.read_holding_registers(REG_ADDR_DRIVE_MILLIAMPS, 1).await??;
-    println!("new val read_rsp: {read_rsp:?}");
+    let new_ma_val = read_rsp[0];
+    println!(" prec old: {original_ma_val:?} new: {new_ma_val:?}");
 
-    sleep(Duration::from_millis(500)).await;
+    sleep(Duration::from_millis(250)).await; // TODO sufficient settling time?
 
     let read_rsp: Vec<u16> = ctx.read_holding_registers(REG_ADDR_MONITOR_MILLIAMPS, 1).await??;
-    let actual_ma = (read_rsp[0] as f32)/10.0;
-    println!("actual current: {actual_ma:?} mA");
+    let actual_ma = (read_rsp[0] as f32)/10.0;// precision is 0.1 mA
+    println!(" prec measured: {actual_ma:?} mA");
 
     Ok(())
 }
@@ -188,64 +187,75 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // set_one_modbus_node_id(tty_path, baud_rate, REG_NODEID_PYRO_CURR_GEN, NODEID_DEFAULT, NODEID_PYRO_CURR_GEN).await?;
 
     let builder = tokio_serial::new(tty_path, baud_rate);
+    // we first create a Context attached to the default Modbus node ID (1) which in theory should not be in use
+    // unless there's a fresh un-configured device attached to the bus
+    // let mut ctx: client::Context = rtu::attach_slave(SerialStream::open(&builder).unwrap(), Slave(NODEID_DEFAULT));
+    let mut ctx: client::Context = rtu::attach(SerialStream::open(&builder).unwrap());
 
-    println!("Check IV ADC... ");
-    let mut ctx_iv_adc: client::Context = rtu::attach_slave(SerialStream::open(&builder).unwrap(), Slave(NODEID_IV_ADC));
-    let read_rsp: Vec<u16> = ctx_iv_adc.read_holding_registers(REG_NODEID_IV, 3).await??;
-    println!(" IV_ADC CFG ({REG_NODEID_IV:?})[3]: {read_rsp:?}");
-    let iv_adc_vals: Vec<u16> = ctx_iv_adc.read_holding_registers(REG_IV_ADC_2CH_VALS, 4).await??;
-    println!(" IV_ADC VALS ({REG_IV_ADC_2CH_VALS:?})[4]: {iv_adc_vals:?}");
-    let ch1_value = registers_to_i32(&iv_adc_vals, 0);
-    let verified_volts = (ch1_value as f32) / 10000.0; // resolution is 0.1 mV for 10V range
-    let ch2_value = registers_to_i32(&iv_adc_vals, 2);
-    let verified_milliamps = (ch2_value as f32)/ 10.0; // resolution is 0.1 mA for 5A range
-    println!(" IV ADC ch1_value: {ch1_value:?} = {verified_volts:?} V");
-    println!(" IV ADC ch2_value: {ch2_value:?} = {verified_milliamps:?} mA");
+    let mut cur_target_milliamps = 1.0f32;
+    loop { 
+        println!("Check IV ADC... ");
+        ctx.set_slave(Slave(NODEID_IV_ADC));
+        // let mut ctx_iv_adc: client::Context = rtu::attach_slave(SerialStream::open(&builder).unwrap(), Slave(NODEID_IV_ADC));
+        let read_rsp: Vec<u16> = ctx.read_holding_registers(REG_NODEID_IV, 3).await??;
+        println!(" IV_ADC CFG ({REG_NODEID_IV:?})[3]: {read_rsp:?}");
+        let iv_adc_vals: Vec<u16> = ctx.read_holding_registers(REG_IV_ADC_2CH_VALS, 4).await??;
+        println!(" IV_ADC VALS ({REG_IV_ADC_2CH_VALS:?})[4]: {iv_adc_vals:?}");
+        let ch1_value = registers_to_i32(&iv_adc_vals, 0);
+        let verified_volts = (ch1_value as f32) / 10000.0; // resolution is 0.1 mV for 10V range
+        let ch2_value = registers_to_i32(&iv_adc_vals, 2);
+        let verified_milliamps = (ch2_value as f32)/ 10.0; // resolution is 0.1 mA for 5A range
+        println!(" IV ADC ch1_value: {ch1_value:?} = {verified_volts:?} V");
+        println!(" IV ADC ch2_value: {ch2_value:?} = {verified_milliamps:?} mA");
 
-    // println!("Disconnecting IV ADC");
-    ctx_iv_adc.disconnect().await?;
+        // Let the bus settle before connecting to a different node
+        sleep(Duration::from_millis(100)).await;
 
-    println!("Check Dual TK... ");
-    let mut ctx_dual_tk: client::Context = rtu::attach_slave(SerialStream::open(&builder).unwrap(), Slave(NODEID_DUAL_TK));
-    let read_rsp: Vec<u16> = ctx_dual_tk.read_holding_registers(REG_NODEID_TK, 1).await??;
-    println!(" Dual TK node ID: {:?}", read_rsp);
+        println!("Check Dual TK... ");
+        ctx.set_slave(Slave(NODEID_DUAL_TK));
+        // let read_rsp: Vec<u16> = ctx.read_holding_registers(REG_NODEID_TK, 1).await??;
+        // println!(" Dual TK node ID: {:?}", read_rsp);
 
-    let cfg_rsp: Vec<u16> = ctx_dual_tk.read_holding_registers(0x20, 3).await??;
-    println!(" 0x20 cfg_rsp: {:?}", cfg_rsp);
+        let cfg_rsp: Vec<u16> = ctx.read_holding_registers(0x20, 3).await??;
+        println!(" 0x20 cfg_rsp: {:?}", cfg_rsp);
 
-    let ch_valid_rsp: Vec<u16> = ctx_dual_tk.read_holding_registers(0x10, 2).await??;
-    println!(" 0x10 ch_valid_rsp: {:?}", ch_valid_rsp);
-    let ch1_tk_conn: bool = ch_valid_rsp[0] == 0; // 0: The thermocouple is connected, 1: The thermocouple is not connected
-    let ch2_tk_conn: bool = ch_valid_rsp[1] == 0;
+        let ch_valid_rsp: Vec<u16> = ctx.read_holding_registers(0x10, 2).await??;
+        // println!(" 0x10 ch_valid_rsp: {:?}", ch_valid_rsp);
+        let ch1_tk_conn: bool = ch_valid_rsp[0] == 0; // 0: The thermocouple is connected, 1: The thermocouple is not connected
+        let ch2_tk_conn: bool = ch_valid_rsp[1] == 0;
 
-    // TODO use reg address consts
-    // example of reading all the dual TK registers:
-    let tk_resp: Vec<u16> = ctx_dual_tk.read_holding_registers(0x00, 2).await??;
-    println!(" 0x00 temp_rsp: {:?}", tk_resp);
-    let ch1_tk_val: f32 = (tk_resp[0] as f32) / 10.0; // resolution is 0.1 °C
-    let ch2_tk_val: f32 = (tk_resp[1] as f32) / 10.0; // resolution is 0.1 °C
+        // TODO use reg address consts
+        // example of reading all the dual TK registers:
+        let tk_resp: Vec<u16> = ctx.read_holding_registers(0x00, 2).await??;
+        // println!(" 0x00 temp_rsp: {:?}", tk_resp);
+        let ch1_tk_val: f32 = (tk_resp[0] as f32) / 10.0; // resolution is 0.1 °C
+        let ch2_tk_val: f32 = (tk_resp[1] as f32) / 10.0; // resolution is 0.1 °C
 
-    if ch1_tk_conn {
-        println!("TK1 connected, {ch1_tk_val:?} °C");
+        if ch1_tk_conn {
+            println!(" TK1 valid: {ch1_tk_val:?}°C");
+        }
+        if ch2_tk_conn {
+            println!(" TK2 valid: {ch2_tk_val:?}°C");
+        }
+        
+        // Let the bus settle before connecting to a different node
+        sleep(Duration::from_millis(100)).await;
+
+        println!("Check Prec Curr ... ");
+        ctx.set_slave(Slave(NODEID_PREC_CURR_SRC)); 
+
+        set_precision_current_drive(&mut ctx, cur_target_milliamps).await?;
+        cur_target_milliamps += 1.0;
+        if cur_target_milliamps > 20.0 { cur_target_milliamps = 0.1; }
+
+        // Let the bus & current settle before connecting to a different node
+        sleep(Duration::from_millis(250)).await;
+
+        // TODO handle events that would lead to shutting down eg current source
     }
-    if ch2_tk_conn {
-        println!("TK2 connected, {ch2_tk_val:?} °C");
-    }
 
 
-
-    // println!("Disconnecting Dual TK");
-    ctx_dual_tk.disconnect().await?;
-
-    println!("Check Prec Curr Src... ");
-    let mut ctx_prec_curr: client::Context = rtu::attach_slave(SerialStream::open(&builder).unwrap(), Slave(NODEID_PREC_CURR_SRC)); 
-    let read_rsp: Vec<u16> = ctx_prec_curr.read_holding_registers(REG_NODEID_PREC_CURR, 1).await??;
-    println!(" read_rsp: {:?}", read_rsp);
-
-    set_precision_current_drive(&mut ctx_prec_curr, 5.7).await?;
-
-    // println!("Disconnecting Prec Curr Src");
-    ctx_prec_curr.disconnect().await?;
+    ctx.disconnect().await?;
 
     Ok(())
 }
