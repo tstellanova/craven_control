0e302f780a8405c52aa20bdcc41994cf784ec69a
Todd Stellanova <tstellanova@users.noreply.github.com>
2026-03-15 17:23:23 -0700

    allup10 prep: detect smaller gap between eleco_dma and eleco_rma

--- src/bin/all_up.rs
+++ src/bin/all_up.rs
@@ -104,14 +104,22 @@ async fn enumerate_required_modules(ctx: &mut tokio_modbus::client::Context) -> 
     Ok(())
 }
 
-fn current_from_current_density(density: f32) -> f32 
+fn current_from_current_density_ma(density_ma_cm2: f64, wire_od_mm: f64, wire_len_mm: f64) -> f32 
 {
-    // 2 * pi * r * h
-    // assume 0.8 mm wire OD, 0.4 mm radius, 10 mm height
-    const ELECTRODE_SA_FACTOR: f64 = (2. * std::f64::consts::PI) * (0.4E-3 * 10E-3); 
-
-    (density as f64 * ELECTRODE_SA_FACTOR) as f32
+    // Surface Area = PI * diameter * length 
+    // For 0.8 mm OD, 10 mm long, outer cylinder SA = 0.251 cm2
+    // 200 mA/cm2 * 0.251 = 50.265 mA
+    // For 0.8 mm OD,  3 mm long, SA = 0.024 cm2, current = 4.8 mA
+    let electrode_surface_area: f64 = std::f64::consts::PI * (wire_od_mm/10. * wire_len_mm/10.); 
+    (density_ma_cm2  * electrode_surface_area) as f32
 }
+
+enum DrivePhase {
+    Init = 0,
+    Anchoring = 1, /// Attach initial carbon atoms to cathode surface
+    Growth = 2, /// Growth of carbon chains between electrodes
+    Done = 3,
+} 
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -141,15 +149,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     const PYRO_CELSIUS_RANGE:f32 = MAX_PYRO_TEMP_C - MIN_PYRO_TEMP_C;
     const MILLIAMPS_PER_CELSIUS:f32 = (MAX_PYRO_MA - MIN_PYRO_MA)/PYRO_CELSIUS_RANGE; 
     const PYRO_SIM_MA_CORRECTION: f32 = -0.375; // required to correct current loop value seen by induction heater
-    let mut cur_core_temperature = 20.;
-    let mut pyro_loop_sim_ma = MIN_PYRO_MA;
-    let mut pyro_loop_actual_ma = 0.;
+    const NOMINAL_GROWTH_MA: f32 = 5.;
+    let mut cur_core_temperature: f64 = 20.;
+    let mut pyro_sim_ma = MIN_PYRO_MA;
+    let mut pyro_actual_ma = 0.;
+    let mut last_pyro_sim_ma = 3.;
     let mut eleco_dma = 0.; // electrode drive current (a value we set)
     let mut last_eleco_dma = 0.; // prior loop electrode drive current
     let mut eleco_rma = 0.; // electrode reported drive current (current driver provides this)
     let mut prior_elecm_volts = 0.; //prior measured volts across electrodes
     let mut prior_elecm_ma = 0.;
-    let mut drive_phase = 0;
+    let mut drive_phase = DrivePhase::Init;
     let mut anchoring_phase_start = 0;
     let mut growth_phase_start = 0;
     let mut constant_current_target_ma = 0.;
@@ -164,7 +174,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         r.store(false, Ordering::SeqCst);
     }).expect("Error setting Ctrl-C handler");
 
-    let mut last_pyro_sim_ma = 3.;
+    // 200 mA / cm^2 is ideal according to Y. Chen et al
+    // const IDEAL_CURRENT_DENSITY_MA_CM2: f64 = 200.;
+    // let ideal_current = current_from_current_density_ma(IDEAL_CURRENT_DENSITY_MA_CM2, 0.8, 10.);
+    // println!("IDEAL_CURRENT: {ideal_current:?} mA");
 
     while running.load(Ordering::SeqCst) { 
 
@@ -181,61 +194,55 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             else { MAX_PYRO_TEMP_C as f32};
         // println!("TK1 {tk1_c:?}°C TK2 {tk2_c:?}°C avg: {avg_core_tk_c:?}°C");
 
-        pyro_loop_sim_ma = MIN_PYRO_MA + MILLIAMPS_PER_CELSIUS * avg_core_tk_c  + PYRO_SIM_MA_CORRECTION; //TODO recalibrate instead??
-        if pyro_loop_sim_ma < 4.0 { pyro_loop_sim_ma = 4.0 };
+        pyro_sim_ma = MIN_PYRO_MA + MILLIAMPS_PER_CELSIUS * avg_core_tk_c  + PYRO_SIM_MA_CORRECTION; //TODO recalibrate instead??
+        if pyro_sim_ma < 4.0 { pyro_sim_ma = 4.0 };
         // Setting a new current value may cause a short dropout in current:
         // we don't set a new current value unless it's significantly different than previous
-        if abs_diff_ne!(last_pyro_sim_ma, pyro_loop_sim_ma, epsilon = 0.01) {
+        if abs_diff_ne!(last_pyro_sim_ma, pyro_sim_ma, epsilon = 0.01) {
             sleep(Duration::from_millis(125)).await;
-            set_pyro_420ma_drive(&mut ctx, pyro_loop_sim_ma).await?;
-            last_pyro_sim_ma = pyro_loop_sim_ma;
+            set_pyro_420ma_drive(&mut ctx, pyro_sim_ma).await?;
+            last_pyro_sim_ma = pyro_sim_ma;
         }
         sleep(Duration::from_millis(125)).await;
-        pyro_loop_actual_ma = read_pyro_420ma_value(&mut ctx).await?;
-        // if abs_diff_ne!(pyro_loop_sim_ma, pyro_loop_actual_ma, epsilon = 0.01) {
-        //     println!("pyro sim {pyro_loop_sim_ma:?} mA actual {pyro_loop_actual_ma:?} mA");
-        // }
+        pyro_actual_ma = read_pyro_420ma_value(&mut ctx).await?;
 
         // recalculate ideal electrode drive current:
         // calculate current density for about 10 mm long, 0.7 mm average OD wires
-        if avg_core_tk_c > 720. && avg_core_tk_c < 820. {
-            // let current_density = 0.7 * 100. * 100.; // ideally around 0.7 A/cm^2 == 7000 A/m^2
-            // eleco_ma = current_from_current_density(current_density);
-            // We expected voltages between 0.8 and 2.0
+        if avg_core_tk_c > 740. && avg_core_tk_c < 780. {
 
             match drive_phase {
-                0 => {
-                    drive_phase = 1;
-                    eleco_dma = 12.;
+                DrivePhase::Init => {
+                    drive_phase = DrivePhase::Anchoring;
+                    eleco_dma = 5.;
                     anchoring_phase_start = chrono::Utc::now().timestamp();
                     println!("start anchor phase at: {anchoring_phase_start:?}");
                 }
-                1 => { // anchoring phase -- constant voltage
-                    if prior_elecm_volts > 1.5 {
+                DrivePhase::Anchoring => { // anchoring phase -- constant voltage
+                    if prior_elecm_volts > 1.2 {
                         eleco_dma -= 0.05;
                     }
-                    else if prior_elecm_volts < 1.4 {
+                    else if prior_elecm_volts < 1.0 {
                         eleco_dma += 0.05;
                     }
                     let current_gap = last_eleco_dma - eleco_rma;
-                    if current_gap >= 2.0 {
+                    if current_gap >= 0.3 {
                         println!("end anchor phase with eleco dma {last_eleco_dma:?} rma {eleco_rma:?}");
                         // TODO verify: transition to the next phase once we've achieved target voltage
-                        constant_current_target_ma = last_eleco_dma;
+                        constant_current_target_ma = eleco_rma + 0.1;
                         eleco_dma = constant_current_target_ma;
-                        drive_phase = 2;
+                        drive_phase = DrivePhase::Growth;
                         growth_phase_start = chrono::Utc::now().timestamp();
                         println!("start growth phase at: {growth_phase_start:?}");
                     }
                 }
-                2 => {  // whisker growth phase: constant current
+                DrivePhase::Growth => {  // whisker growth phase: constant current
                     eleco_dma = constant_current_target_ma;
 
                     // Terminate the current drive if we determine that resistance is close to zero (indicating
                     // that the electrode-electrode gap has been bridged by conductive material).
                     if prior_inter_electrode_resistance < MIN_INTER_ELECTRODE_OHMS  {
                         println!("end growth phase with V {prior_elecm_volts:?} R {prior_inter_electrode_resistance:?}");
-                        drive_phase = 3;
+                        drive_phase = DrivePhase::Done;
                         eleco_dma = 0.;
                         let growth_phase_end = chrono::Utc::now().timestamp();
                         println!("end growth phase at: {growth_phase_end:?}");
@@ -281,7 +288,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             tk1_c, tk2_c, avg_core_tk_c,
             eleco_dma, eleco_rma, elecm_ma,
             elecm_volts, inter_electrode_resistance,
-            pyro_loop_sim_ma as f32, pyro_loop_actual_ma as f32
+            pyro_sim_ma as f32, pyro_actual_ma as f32
             );
         println!("{}",log_line);
         writeln!(  csv_writer,"{}",  log_line)?;
